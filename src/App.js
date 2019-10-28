5d5d9d18764ad575d7fc23a76cd8c48b3f98482e
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-28 19:29:01 -0300

    feat: Add reactotron configurations

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import './config/ReactotronConfig';
 import GlobalStyles from './styles/global';
 import Routes from './routes';
-
 import Header from './components/Header';
 
 import store from './store';
