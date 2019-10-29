67a3f0d6c36edce7e88e01d0213cf68d053806c5
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-29 13:36:42 -0300

    chore: Create base project
    - Add ESLint + Prettier + EditorConfig
    - Add and setting Reactotron

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+import './config/ReactotronConfig';
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Rocketshoes mobile</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
