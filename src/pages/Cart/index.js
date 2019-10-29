ea37bee58f54cde0b002bce23cc7e8b076b634bb
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-10-28 21:36:33 -0300

    feat: Calculate total and subtotal to products

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -10,8 +10,9 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 import * as CartActions from '../../store/modules/cart/actions';
+import { formatPrice } from '../../util/format';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ total, cart, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -54,7 +55,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$258,80</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button type="button">
@@ -75,7 +76,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$523,25</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -85,11 +86,21 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 Cart.propTypes = {
   // cart: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   cart: PropTypes.shape([]).isRequired,
+  total: PropTypes.string.isRequired,
   removeFromCart: PropTypes.func.isRequired,
+  updateAmount: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = dispatch =>
